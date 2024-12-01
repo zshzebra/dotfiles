a2ea0c46703e6c397cd0970734ccbdfe05bb513d
pyscripter99 <ryder@retzlaff.family>
2024-12-01 12:18:14 +1100

    Refactor mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,8 +4,11 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- Normal Mode
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
 map("n", "<leader>ft", "<cmd> NvimTreeToggle <cr>")
+
+-- Insert Mode
+map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
